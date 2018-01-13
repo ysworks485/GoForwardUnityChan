01cc61db60f55b9295d0f579fdffebbe089e813a
LAPTOP-8GD7ET5M\ym485 <ym485r@yahoo.co.jp>
2018-01-14 01:50:44 +0900

    Lesson7 課題提出（Cubeが地面またはCube同士で接触した時に音が出るようにする）
    CubeSoundEffectクラスを追加して、接触を判定し音を出す

---
+++ Assets/CubeSoundEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSoundEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    ///////////////////////////////////////////////////////////
+    // Lesson7 課題作成（Cubeが接触したら音を出す）
+    ///////////////////////////////////////////////////////////
+
+    // 地面またはCube同士で接触した場合の処理
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag != "UnityChan") {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+    // アニメーションをするためのコンポーネントを入れる
+    Animator animator;
+    // Unityちゃんを移動させるコンポーネントを入れる
+    Rigidbody2D rigid2D;
+    // 地面の位置
+    private float groundLevel = -3.0f;
+    // ジャンプの速度の減衰
+    private float dump = 0.8f;
+    // ジャンプの速度
+    float jumpVelocity = 20;
+    // ゲームオーバーになる位置
+    private float deadLine = -9;
+
+	// Use this for initialization
+	void Start () {
+        // アニメータのコンポーネントを取得する
+        this.animator = GetComponent<Animator>();
+        // RigidBody2Dのコンポーネントを取得する
+        this.rigid2D = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 走るアニメーションを再生するために、Animatorのパラメータを調節
+        this.animator.SetFloat("Horizontal", 1);
+
+        // 着地しているかどうか調べる
+        bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+        this.animator.SetBool("isGround", isGround);
+
+        // ジャンプ状態の時、ボリュームをゼロにする
+        GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+		
+        // 着地状態でクリックされた場合
+        if (Input.GetMouseButtonDown(0) && isGround)
+        {
+            // 上方向の力をかける
+            this.rigid2D.velocity = new Vector2(0, this.jumpVelocity);
+        }
+
+        // クリックをやめたら上方向への速度を減衰する
+        if (Input.GetMouseButton(0) == false) {
+            if (this.rigid2D.velocity.y > 0) {
+                this.rigid2D.velocity *= this.dump;
+            }
+        }
+
+        // デッドラインを超えた場合、ゲームオーバーにする
+        if (transform.position.x < this.deadLine) {
+            // UIControllerのGameOver関数を呼び出して画面上にGameOverと表示
+            GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
+
+            // ユニティちゃんを破棄
+            Destroy(gameObject);
+        }
+	}
+}
